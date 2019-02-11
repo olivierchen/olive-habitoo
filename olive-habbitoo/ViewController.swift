1577c6dd17b702ab2afda33d13b2aa68ad20ca16
Olivier CHEN <olivierchen@olivierdemacbook-pro.local>
2019-02-11 19:50:51 +0800

    Initial Commit

---
+++ olive-habbitoo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  olive-habbitoo
+//
+//  Created by Olivier CHEN on 2019/2/11.
+//  Copyright © 2019 Olivier CHEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ olive-habbitooTests/olive_habbitooTests.swift
@@ -0,0 +1,34 @@
+//
+//  olive_habbitooTests.swift
+//  olive-habbitooTests
+//
+//  Created by Olivier CHEN on 2019/2/11.
+//  Copyright © 2019 Olivier CHEN. All rights reserved.
+//
+
+import XCTest
+@testable import olive_habbitoo
+
+class olive_habbitooTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
